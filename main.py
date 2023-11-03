9826bb3966c279a3575c995da093def6faf7e52d
MichailoZaichenko <michailo.zaichenko@gmail.com>
2023-11-03 22:44:36 +0200

    trying to turn it into bot

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import logging
 from os import environ as env
-
+import g4f
 import telebot
 import openai
-
+from env import BOT_API_KEY, OPENAI_API_KEY, USER_KEY
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
-bot = telebot.TeleBot('BOT_API_KEY')
-openai.api_key = ("OPENAI_API_KEY")
-user_id = int('USER_KEY')
+bot = telebot.TeleBot(BOT_API_KEY)
+openai.api_key = OPENAI_API_KEY
+user_id = USER_KEY
 
 
 @bot.message_handler(func=lambda message: True)
@@ -18,47 +18,10 @@ def get_response(message):
   if int(message.chat.id) != user_id:
     bot.send_message("This bot is not for public but private use only.")
   else:
-    response = ""
-    if message.text.startswith(">>>"):
-      # Use Codex API for code completion
-      response = openai.Completion.create(
-        engine="code-davinci-002",
-        prompt=f'```\n{message.text[3:]}\n```',
-        temperature=0,
-        max_tokens=4000,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=["\n", ">>>"],
-      )
-    else:
-      # Use GPT API for text completion
-      # Check if the question is about code or not
-      if "code" in message.text.lower() or "python" in message.text.lower():
-        # Use Codex API for code-related questions
-        response = openai.Completion.create(
-          engine="code-davinci-002",
-          prompt=f'"""\n{message.text}\n"""',
-          temperature=0,
-          max_tokens=4000,
-          top_p=1,
-          frequency_penalty=0,
-          presence_penalty=0,
-          stop=['"""'],
-        )
-      else:
-        # Use GPT API for non-code-related questions
-        response = openai.Completion.create(
-          engine="text-davinci-003",
-          prompt=f'"""\n{message.text}\n"""',
-          temperature=0,
-          max_tokens=2000,
-          top_p=1,
-          frequency_penalty=0,
-          presence_penalty=0,
-          stop=['"""'],
-        )
-
+    response = g4f.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": message}],
+    )
     bot.send_message(message.chat.id, f'{response["choices"][0]["text"]}', parse_mode="None")
 
 bot.infinity_polling()
